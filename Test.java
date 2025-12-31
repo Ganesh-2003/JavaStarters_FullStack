b21a2516d2eb56167fa762ccb94b35e6510271b3
Ganesh-2003 <ganeshvsp12@gmail.com>
2025-12-31 10:21:08 +0530

    Intial commit

--- Test.java
+++ Test.java
@@ -1,5 +1,13 @@
 public class Test {
+
+    int x = 10;
+
     public static void main(String[] args){
         System.out.println("Ganesh Puppala");
     }
+
+    public static void main2(String[] args) {
+        System.out.print("Hello World");
+    }
+
 }
